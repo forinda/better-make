e064aa17af3a19695f87b5d242511d1ef42e0eb0
Felix Orinda <forinda82@gmail.com>
2022-10-28 06:23:57 +0300

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Better make for us</div>
+    <div className='text-center py-4 uppercase text-green-600'>Better make for us</div>
   )
 }
 
